9443efa22d4e3eec2d351bd6b602857d73c29c9d
승준 이 <tmdwns7800@naver.com>
2021-10-10 12:14:00 +0900

    초기 커밋.

---
+++ BackTestingFinal/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using TradingLibrary;
+
+namespace BackTestingFinal
+{
+    public partial class Form1 : Form
+    {
+        BackTesting backTesting;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            backTesting = new BackTesting(this, @"C:\Users\tmdwn\source\repos\CybosPlus\DaySticks.db", 0.3m);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Left:
+                    backTesting.beforeButton.PerformClick();
+                    return true;
+
+                case Keys.Right:
+                    backTesting.afterButton.PerformClick();
+                    return true;
+
+                default:
+                    return base.ProcessCmdKey(ref msg, keyData);
+            }
+        }
+    }
+}
