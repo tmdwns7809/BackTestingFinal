3972db1c95685578796d50c982f212344c89e7e0
승준 이 <tmdwns7800@naver.com>
2022-01-23 11:14:02 +0900

    1113

--- BackTestingFinal/BackTesting.cs
+++ BackTestingFinal/BackTesting.cs
@@ -95,7 +95,7 @@ namespace BackTestingFinal
 
         Dictionary<ChartValues, SQLiteConnection> DBDic = new Dictionary<ChartValues, SQLiteConnection>();
 
-        public BackTesting(Form form, bool isJoo) : base(form, isJoo, 11)
+        public BackTesting(Form form, bool isJoo) : base(form, isJoo, 7)
         {
             sticksDBpath = BaseSticksDB.path;
             sticksDBbaseName = BaseSticksDB.BaseName;
