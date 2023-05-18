89a20f5502736af4f38eda4ecf3c5c5279d32fee
sj <sj@DESKTOP-6F4NEMJ>
2023-05-19 05:37:27 +0900

    0537

--- BackTestingFinal/BackTesting.cs
+++ BackTestingFinal/BackTesting.cs
@@ -142,7 +142,7 @@ namespace BackTestingFinal
 
         int minituesInADay = BaseChartTimeSet.OneDay.seconds / BaseChartTimeSet.OneMinute.seconds;
 
-        public BackTesting(Form form, bool isJoo) : base(form, isJoo, 8m)
+        public BackTesting(Form form, bool isJoo) : base(form, isJoo, 8.2m)
         {
 
             sticksDBpath = BaseSticksDB.path;
